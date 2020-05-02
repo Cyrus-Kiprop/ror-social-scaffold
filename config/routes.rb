fc7d1952764eb5c0b5151565a1d14528258a5d6e
kip <cyruskiprop254@gmail.com>
2020-05-02 15:59:07 +0300

    add more friendship routes to support creation and deletion of friend requests

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   get 'friendships/index'
   get 'friendships/show'
   get 'friendships/create'
-  get 'friendships/destroy'
+  patch 'friendships/update/:id', to: 'friendships#update', as: 'friendships_update'
+  delete 'friendships/destroy/:id', to: 'friendships#destroy', as: 'friendships_destroy'
   root 'posts#index'
 
   devise_for :users
 
+  resources :friendships, only: [:index, :show, :create, :destroy, :update]
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
